31313e30a95112d73f1194bd96f551cdbd838c4c
checcomi <checcomi@gmail.com>
2020-09-07 11:52:19 +0100

    close if statement

--- Rsrc/settings.r
+++ Rsrc/settings.r
@@ -100,8 +100,9 @@ splitRun <- FALSE
 # Range/number of split parts. NOTICE: Code doesn't adjust number of split parts by merely 
 # adjusting this variable. If number of parts needs to be changed from 4, both 1.1 and 1.9 
 # need to be modified.
-if (splitRun){
+if(splitRun){
   splitRange <- 1:4
+}
 
 ####thresholds for variables to reset stand from plantation
 maxDens <- 10000
