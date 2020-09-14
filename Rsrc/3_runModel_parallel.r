ab92860c0e16de2c18831a032973a30a8a20857d
KristiinaKoivu <54938653+KristiinaKoivu@users.noreply.github.com>
2020-09-14 15:53:05 +0300

    Update 3_runModel_parallel.r
    Enable running from github scripts

--- Rsrc/3_runModel_parallel.r
+++ Rsrc/3_runModel_parallel.r
@@ -1,8 +1,11 @@
-### Run settings
-source("/scratch/project_2000994/PREBASruns/assessCarbon/Rsrc/mainSettings.r")
+# Run settings (if modifiedSettings is not set to TRUE in batch job script, default settings from Github will be used)
+source_url("https://raw.githubusercontent.com/ForModLabUHel/satRuns/master/Rsrc/settings.r")
+if(modifiedSettings) {
+  source("/scratch/project_2000994/PREBASruns/assessCarbon/Rsrc/mainSettings.r") # in CSC
+}
+
 setwd(generalPath)
 ###check and create output directories
-setwd(generalPath)
 mkfldr <- paste0("output/","init",startingYear,"/st",siteTypeX)
 if(!dir.exists(file.path(generalPath, mkfldr))) {
   dir.create(file.path(generalPath, mkfldr), recursive = TRUE)
